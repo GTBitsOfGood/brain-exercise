4fda4f2ee2813355c3a5cda7db0d97f68ca8530f
sjacob2019 <55889374+sjacob2019@users.noreply.github.com>
2020-06-07 15:09:09 -0400

    Updating App.js

--- App.js
+++ App.js
@@ -18,11 +18,3 @@ export default function App() {
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
