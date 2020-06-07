6d23452703550a316903b87b5afd3ada353f650b
sjacob2019 <55889374+sjacob2019@users.noreply.github.com>
2020-06-07 14:51:10 -0400

    Updated Files

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from './screens/HomeScreen';
 import GameScreen from './screens/GameScreen';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <HomeScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="GameScreen" component={GameScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
