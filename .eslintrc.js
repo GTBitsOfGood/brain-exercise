6a36968eeb5c0d58348c1aa5273d968387b83e81
Arthelon <hsing.daniel@gmail.com>
2020-06-07 14:56:28 +0800

    chore: Setup eslint configs
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parser: "babel-eslint", // needed to make babel stuff work properly
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react", "react-native", "prettier", "import"],
+  extends: [
+    "airbnb-base",
+    "prettier",
+    "plugin:react/recommended",
+    "plugin:react-native-a11y/basic",
+  ],
+  rules: {
+    "prettier/prettier": ["error"],
+    "react-native/react-native": true,
+  },
+};
