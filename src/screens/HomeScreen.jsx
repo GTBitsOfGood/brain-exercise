c8eb7fb51fd6ccb6562e11fcd8b5c5474d033293
Jorge Aquino <jaquino6286@gmail.com>
2020-06-08 22:31:48 -0400

    -Add style enhancements to stylesheet in HomeScreen
    -Add settings screen and put its stack.screen into App.js

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -6,28 +6,33 @@ import PropTypes from "prop-types";
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#f5f5f5",
     alignItems: "center",
     justifyContent: "center",
   },
   title: {
-    color: "red",
-    fontSize: 20,
+    color: "black",
+    fontSize: 30,
+    margin: 30,
   },
   button: {
-    color: "red",
-    borderColor: "blue",
+    margin: 30,
   },
 });
 
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>Home Screen</Text>
+      <Text style={styles.title}>Brain Gains</Text>
       <Button
+        title="Games"
         style={styles.button}
-        title="GScreen"
-        onPress={() => navigation.navigate("GameScreen")}
+        onPress={() => navigation.navigate("Games")}
+      />
+      <Button
+        title="Settings"
+        style={styles.button}
+        onPress={() => navigation.navigate("Settings")}
       />
     </View>
   );
