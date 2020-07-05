edff4931cbbb0dd13ec903c54c2a837fdae1b3b0
Fattimo <matt.chen522@gmail.com>
2020-07-05 00:59:33 -0600

    progress bar completed. can be tested on game screen

--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { View, Text } from "react-native";
+import ProgressBar from "./../components/ProgressBar"
 
 function GameScreen() {
   return (
     <View>
       <Text>Game Screen</Text>
+      <ProgressBar
+        seconds={120}
+        red={30}
+        func={2 == 1+1} // function here
+      />
     </View>
   );
 }
