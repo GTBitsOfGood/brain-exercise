16372481c5b6c71aa5bccdef1168b6be74074a01
Jorge Aquino <jaquino6286@gmail.com>
2020-07-06 12:16:26 -0400

    style and navigation functionality

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-// Importing HomeScreens
+// Importing Home Screens
 import HomeScreen from "./src/screens/Home/HomeScreen.jsx";
 import Video from "./src/screens/Home/Video.jsx";
-// Importing SettingsScreens
+// Importing Settings Screens
 import SettingsScreen from "./src/screens/Settings/SettingsScreen.jsx";
 import TimePicker from "./src/screens/Settings/TimePicker.jsx";
 import FontSize from "./src/screens/Settings/FontSize.jsx";
-// Importing GameScreens
+// Importing Game Screens
 import GameOverview from "./src/screens/Game/GameOverview.jsx";
 import GameMaterials from "./src/screens/Game/GameMaterials.jsx";
 import Gameplay from "./src/screens/Game/Gameplay.jsx";
@@ -22,176 +22,100 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+      // Consistent styling across all stacked screens
+      screenOptions={{
+        headerTitleAllowFontScaling: true,
+        headerBackTitleVisible: false,
+
+        headerStyle: {
+          backgroundColor: '#005AA3',
+        },
+
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+
+        {/* Home Screens: */}
         <Stack.Screen
           name="HomeScreen"
           component={HomeScreen}
-          options={{
-            title: "Brain Gains",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+          options={{title: "Brain Gains"}}
         />
         <Stack.Screen
           name="Video"
           component={Video}
           options={{
-            title: "Youtube Video",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+            title: "Youtube Video"}}
         />
 
+        {/* Game Screens */}
         <Stack.Screen
-          name="Game Overview"
+          name="GameOverview"
           component={GameOverview}
           options={{
-            title: "Today's Exercises",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+            title: "Today's Exercises"}}
         />
         <Stack.Screen
-          name="Game Materials"
+          name="GameMaterials"
           component={GameMaterials}
           options={{
-            title: "Game Materials",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+            title: "Game Materials",}}
         />
         <Stack.Screen
           name="Gameplay"
           component={Gameplay}
           options={{
-            title: "Gameplay",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+            title: "Gameplay"}}
         />
         <Stack.Screen
           name="GameplayIntermediate"
           component={GameplayIntermediate}
           options={{
-            title: "Gameplay Intermediate",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+            title: "Gameplay Intermediate"}}
         />
         <Stack.Screen
           name="Pause"
           component={Pause}
           options={{
-            title: "Paused",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+            title: "Paused"}}
         />
         <Stack.Screen
-          name="Finished Screen"
+          name="FinishedScreen"
           component={FinishedScreen}
           options={{
-            title: "Exercises Completed",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+            title: "Exercises Completed"}}
         />
         <Stack.Screen
-          name="Extra Practice"
+          name="ExtraPractice"
           component={ExtraPractice}
           options={{
-            title: "More Exercises",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+            title: "More Exercises"}}
         />
 
+        {/* Settings Screens: */}
         <Stack.Screen
           name="SettingsScreen"
           component={SettingsScreen}
           options={{
             title: "Settings",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+            headerStyle: {backgroundColor: '#2a652c'}}}
         />
         <Stack.Screen
           name="TimePicker"
           component={TimePicker}
           options={{
             title: "Set Time Reminder",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+            headerStyle: {backgroundColor: '#2a652c'}}}
         />
         <Stack.Screen
           name="FontSize"
           component={FontSize}
           options={{
             title: "Font Size",
-            headerStyle: {
-              backgroundColor: "#005AA3",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+            headerStyle: {backgroundColor: '#2a652c'}}}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/Settings/SettingsScreen.jsx
+++ src/screens/Settings/SettingsScreen.jsx
@@ -1,37 +1,68 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Switch, Text } from "react-native";
 import { Button } from "react-native-elements";
 import PropTypes from "prop-types";
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+    alignContent: "center",
     backgroundColor: "#f5f5f5",
-    alignItems: "center",
-    justifyContent: "center",
+    marginVertical: 30,
   },
-  title: {
-    color: "black",
-    fontSize: 30,
-    padding: 30,
+  reminder: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 30,
+    marginHorizontal: 30,
+  },
+  text: {
+    textAlign: "left",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginHorizontal: 30,
+  },
+  subtext: {
+    textAlign: "left",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginHorizontal: 30,
   },
   button: {
-    padding: 20,
-  },
+    alignContent: "space-between",
+    color: "black",
+  }
 });
 
 // Settings Navigation
 function SettingsScreen({ navigation }) {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
-    <View style={styles.root}>
-      <Button
-        title="Time Picker"
-        style={styles.button}
+     <View style={styles.root}>
+       <Text style={styles.text}>Notifications</Text>
+       <View style={styles.reminder}>
+        <Text>Daily Reminder</Text>
+        <Switch
+          trackColor={{ false: "#ffffff", true: "#2a652c" }}
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+          accessibilityRole="switch"
+        />
+      </View>
+      <View style={styles.reminder}>
+        <Text>Set Reminder Time</Text>
+        <Button
+        title="10:00 am"
+        type="outline"
         onPress={() => navigation.navigate("TimePicker")}
       />
+      </View>
       <Button
-        title="Font Size"
-        style={styles.button}
+        buttonStyle={styles.button}
+        title="Font Size                                                 >"
+        type="clear"
+        containerStyle={{ margin: 20 }}
         onPress={() => navigation.navigate("FontSize")}
       />
     </View>
