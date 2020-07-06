8141779b285ffdeeb6825062c2a75e2e29d204e4
sjacob2019 <55889374+sjacob2019@users.noreply.github.com>
2020-07-06 14:34:31 -0400

    Styling

--- src/screens/Settings/FontSize.jsx
+++ src/screens/Settings/FontSize.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import Slider from "react-native-slider";
 import { Button, Slider } from "react-native-elements";
 import { StyleSheet, View, Text } from "react-native";
 
@@ -8,14 +7,21 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: "stretch",
-    justifyContent: "center"
+    justifyContent: "center",
   },
-  slider: {
-    maxHeight: 100,
-  },
-  notText: {
+  sliderWithButton: {
     marginVertical: 160,
-  }
+  },
+  saveButton: {
+    marginVertical: 40,
+    backgroundColor: "#2a652c",
+    borderRadius: 10,
+  },
+  // buttonStyle: {
+  //   flex: 0.01,
+  //   justifyContent: "flex-end",
+  //   marginBottom: 36,
+  // }
 });
 
 export default class FontSize extends React.Component {
@@ -29,19 +35,22 @@ export default class FontSize extends React.Component {
         <Text style={StyleSheet.create({fontSize: this.state.value})}>
           Drag the slider below to make the text on screen smaller or larger. Value: {this.state.value}
         </Text>
-        <View style={styles.notText}>
+        <View style={styles.sliderWithButton}>
           <Slider
             style={styles.slider}
             value={this.state.value}
+            thumbTintColor={"#2a652c"}
             minimumValue={16}
             maximumValue={34}
-            step={1}
+            step={4}
             onValueChange={(value) => this.setState({ value })}
           />
-          <Button
-            style={styles.saveButton}
-            title="Save Changes"
-          />
+          <View style={styles.buttonStyle}>
+            <Button
+              buttonStyle={styles.saveButton}
+              title="Save Changes"
+            />
+          </View>
         </View>
       </View>
     );
