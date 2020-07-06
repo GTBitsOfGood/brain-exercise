248f8d1ed3357e8407b290bdaffb1e09d5e88eb4
Jorge Aquino <jaquino6286@gmail.com>
2020-07-06 18:22:46 -0400

    c

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from 'react-native';
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
