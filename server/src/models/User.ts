dc9b1caa3d082d6e20de9299bc6ff9f09f6fda28
Edmund Xin <edmund@gatech.edu>
2022-11-13 01:10:06 -0800

    Fix user schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,48 +1,26 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 export type UserDocument = mongoose.Document & {
-    name: string;
-    phoneNumber: number;
-    birthdate: Date;
-    accessToken: string;
-    email: string;
-    username: string;
-    password: string;
-}
-
-const userSchema = new mongoose.Schema<UserDocument>({
-    name: { type: String, required: true },
-    phoneNumber: { type: Number, required: true },
-    birthdate : { type: Date, required: false },
-    accessToken : { type: String, required: true, unique: true },
-    email: { type: String, required: true },
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-}, { timestamps: true });
-
-userSchema.pre('save', function(next) {
-    const user = this;
-
-    bcrypt.genSalt(10, function(err, salt) {
-        if (err) return next(err);
-
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
-    });
-});
-
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
+  name: string;
+  phoneNumber: number;
+  birthdate: Date;
+  accessToken: string;
+  secondaryContactName: string;
+  secondaryPhoneNumber: string;
 };
 
-export const User = mongoose.model<UserDocument>('users', userSchema);
+const userSchema = new mongoose.Schema<UserDocument>(
+  {
+    name: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
+    birthdate: { type: Date, required: false },
+    accessToken: { type: String, required: true, unique: true },
+    secondaryContactName: { type: String, required: false },
+    secondaryPhoneNumber: { type: String, required: false },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model<UserDocument>("users", userSchema);
