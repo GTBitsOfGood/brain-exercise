5eb47d37b0a5be6a6cfdc19f66688f97574d6ebb
Jay Nagpaul <jaynagpaul@users.noreply.github.com>
2023-02-14 16:48:47 -0500

    JWT additions (forgot to mention)

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -1,17 +1,36 @@
-import express from 'express';
-
-const passport = require('passport');
-require('./passportConfig')(passport);
+import express from "express";
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+require("./passportConfig")(passport);
 const router = express.Router();
 
-router.route("/google").get(passport.authenticate("google", { scope: ["email", "profile"] }));
+router
+  .route("/google")
+  .get(passport.authenticate("google", { scope: ["email", "profile"] }));
 
-router.route("/google/callback").get(passport.authenticate("google", { session: false }), (req, res) => {
-    res.redirect("/");
-});
+router
+  .route("/google/callback")
+  .get(passport.authenticate("google", { session: false }), (req, res) => {
+    jwt.sign(
+      // @ts-ignore
+      { user: req.user },
+      "secretKey",
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) {
+          return res.json({
+            token: null,
+          });
+        }
+        res.json({
+          token,
+        });
+      }
+    );
+  });
 
 router.route("/").get((req, res) => {
-    res.send("this is a test route");
+  res.send("this is a test route");
 });
 
 export default router;
--- server/src/routes/auth/passportConfig.ts
+++ server/src/routes/auth/passportConfig.ts
@@ -1,31 +1,52 @@
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
-import { User, UserDocument } from '../../models/User';
-import { CLIENT_ID, CLIENT_SECRET } from '../../util/secrets';
+import { User } from "../../models/User";
+import { CLIENT_ID, CLIENT_SECRET, SESSION_SECRET } from "../../util/secrets";
+const JwtStrategy = require("passport-jwt").Strategy;
+const { ExtractJwt } = require("passport-jwt");
 
 module.exports = (passport) => {
-    passport.use(new GoogleStrategy({
+  passport.use(
+    new GoogleStrategy(
+      {
         clientID: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         callbackURL: "http://localhost:3000/auth/google/callback",
-        passReqToCallback : true
-    },
-    async (request, accessToken, refreshToken, profile, done) => {
+        passReqToCallback: true,
+      },
+      async (request, accessToken, refreshToken, profile, done) => {
         try {
-            let existingUser = await User.findOne({ 'googleId': profile.id });
-            if (existingUser) {
-                return done(null, existingUser);
-            }
-            console.log('Creating new user...');
-            const newUser = new User({
-                name: profile.displayName,
-                googleId: profile.id,
-                accessToken: accessToken
-            });
-            await newUser.save();
-            return done(null, newUser);
-            } catch (error) {
-                return done(error, false)
-            }
+          let existingUser = await User.findOne({ googleId: profile.id });
+          if (existingUser) {
+            return done(null, existingUser);
+          }
+          console.log("Creating new user...");
+          const newUser = new User({
+            name: profile.displayName,
+            googleId: profile.id,
+            accessToken: accessToken,
+          });
+          await newUser.save();
+          return done(null, newUser);
+        } catch (error) {
+          return done(error, false);
         }
-    ));
-}
+      }
+    )
+  );
+  passport.use(
+    new JwtStrategy(
+      {
+        jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+        secretOrKey: SESSION_SECRET,
+      },
+      async (jwtPayload, done) => {
+        try {
+          const user = jwtPayload.user;
+          done(null, user);
+        } catch (error) {
+          done(error, false);
+        }
+      }
+    )
+  );
+};
