dbb52ea58576537afe17cf22ded7d717fcdc9faa
Mahesh Natamai <44304651+MaheshNat@users.noreply.github.com>
2023-03-02 10:57:02 -0600

    fixed timer is paused after quitting from home screen bug (#171)
    Co-authored-by: Jay Nagpaul <jaynagpaul@users.noreply.github.com>

--- App.js
+++ App.js
@@ -3,53 +3,53 @@ import { NavigationContainer } from "@react-navigation/native";
 import {
   createStackNavigator,
   HeaderBackButton,
-} from "@react-navigation/stack";
-import { Text } from "react-native";
-import { Auth0Provider } from "react-native-auth0";
+} from '@react-navigation/stack';
+import { Text } from 'react-native';
+import { Auth0Provider } from 'react-native-auth0';
 
-import Constants from "expo-constants";
-import axios from "axios";
-import { logAxiosError } from "./src/utils";
+import axios from 'axios';
+import Constants from 'expo-constants';
+import { logAxiosError } from './src/utils';
 
 // Importing temporary login screen
-import Login from "./src/screens/Settings/Login.jsx";
+import Login from './src/screens/Settings/Login.jsx';
 
 // Importing Home Screen
-import HomeScreen from "./src/screens/Home/HomeScreen.jsx";
+import HomeScreen from './src/screens/Home/HomeScreen.jsx';
 
 // Importing Settings Screens
-import SettingsScreen from "./src/screens/Settings/SettingsScreen.jsx";
-import TimePicker from "./src/screens/Settings/TimePicker.jsx";
-import FontSize from "./src/screens/Settings/FontSize.jsx";
-import SoundScreen from "./src/screens/Settings/SoundScreen.jsx";
+import FontSize from './src/screens/Settings/FontSize.jsx';
+import SettingsScreen from './src/screens/Settings/SettingsScreen.jsx';
+import SoundScreen from './src/screens/Settings/SoundScreen.jsx';
+import TimePicker from './src/screens/Settings/TimePicker.jsx';
 
 // Importing Game Screens
-import GameOverview from "./src/screens/Game/GameOverview.jsx";
-import GameMaterials from "./src/screens/Game/GameMaterials.jsx";
-import Gameplay from "./src/screens/Game/Gameplay.jsx";
-import TriviaScreen from "./src/screens/Game/TriviaScreen.jsx";
-import GameplayIntermediate from "./src/screens/Game/GameplayIntermediate.jsx";
-import ReadingIntro from "./src/screens/Game/ReadingIntro.jsx";
-import Pause from "./src/screens/Game/Pause.jsx";
-import FinishedScreen from "./src/screens/Game/FinishedScreen.jsx";
-import ExtraPractice from "./src/screens/Game/ExtraPractice.jsx";
-import ReadingMain from "./src/screens/Game/ReadingMain.jsx";
-import ExercisesCompleted from "./src/screens/Game/ExercisesCompleted.jsx";
-import PromptScreen from "./src/screens/Game/PromptScreen.jsx";
-import WritingIntro from "./src/screens/Game/WritingIntro.jsx";
-import MathIntro from "./src/screens/Game/MathIntro.jsx";
-import TriviaIntro from "./src/screens/Game/TriviaIntro.jsx";
-import PauseButton from "./src/components/PauseButton.jsx";
+import PauseButton from './src/components/PauseButton.jsx';
+import ExercisesCompleted from './src/screens/Game/ExercisesCompleted.jsx';
+import ExtraPractice from './src/screens/Game/ExtraPractice.jsx';
+import FinishedScreen from './src/screens/Game/FinishedScreen.jsx';
+import GameMaterials from './src/screens/Game/GameMaterials.jsx';
+import GameOverview from './src/screens/Game/GameOverview.jsx';
+import Gameplay from './src/screens/Game/Gameplay.jsx';
+import GameplayIntermediate from './src/screens/Game/GameplayIntermediate.jsx';
+import MathIntro from './src/screens/Game/MathIntro.jsx';
+import Pause from './src/screens/Game/Pause.jsx';
+import PromptScreen from './src/screens/Game/PromptScreen.jsx';
+import ReadingIntro from './src/screens/Game/ReadingIntro.jsx';
+import ReadingMain from './src/screens/Game/ReadingMain.jsx';
+import TriviaIntro from './src/screens/Game/TriviaIntro.jsx';
+import TriviaScreen from './src/screens/Game/TriviaScreen.jsx';
+import WritingIntro from './src/screens/Game/WritingIntro.jsx';
 
 // React Redux Persist State
-import useCachedResources from "./src/hooks/useCachedResources";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Provider } from "react-redux";
-import { persistStore } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import { store } from "./src/redux/store";
-import SignUpScreen from "./src/screens/SignUp/SignUp.jsx";
-import StreakLength from "./src/screens/Settings/StreakLength";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import useCachedResources from './src/hooks/useCachedResources';
+import { store } from './src/redux/store';
+import StreakLength from './src/screens/Settings/StreakLength';
+import SignUpScreen from './src/screens/SignUp/SignUp.jsx';
 
 // Time Analytics
 import { AppState } from "react-native";
@@ -61,7 +61,7 @@ Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.allowFontScaling = false;
 
 const config = {
-  animation: "spring",
+  animation: 'spring',
   config: {
     stiffness: 1000,
     damping: 500,
@@ -131,8 +131,8 @@ export default function App() {
   } else {
     return (
       <Auth0Provider
-        domain={"bitsofgood.us.auth0.com"}
-        clientId={"7DAGINdmjDATs8dDqA0c3i6XrPgbe2MT"}
+        domain={'bitsofgood.us.auth0.com'}
+        clientId={'7DAGINdmjDATs8dDqA0c3i6XrPgbe2MT'}
       >
         <Provider store={store}>
           <PersistGate persistor={persistor} loading={null}>
@@ -185,35 +185,35 @@ export default function App() {
                       headerBackTitleVisible: false,
                       headerTitleAllowFontScaling: false,
                       gestureEnabled: false,
-                      headerTintColor: "black",
+                      headerTintColor: 'black',
                       headerLeft: null,
                       headerStyle: {
-                        backgroundColor: "white",
+                        backgroundColor: 'white',
                       },
                       headerTitleStyle: {
-                        fontWeight: "bold",
+                        fontWeight: 'bold',
                         fontSize: 22,
-                        color: "black",
+                        color: 'black',
                       },
                     }}
                   >
                     {/* Home Screens */}
                     <Stack.Screen
-                      name="HomeScreen"
+                      name='HomeScreen'
                       component={signedIn ? HomeScreen : Login}
-                      options={{ title: "Home" }}
+                      options={{ title: 'Home' }}
                     />
 
                     <Stack.Screen
-                      name="SignUpScreen"
+                      name='SignUpScreen'
                       component={SignUpScreen}
-                      options={{ title: "SignUp" }}
+                      options={{ title: 'SignUp' }}
                     />
 
                     {/* Game Screens */}
 
                     <Stack.Screen
-                      name="GameOverview"
+                      name='GameOverview'
                       component={GameOverview}
                       options={({ navigation }) => ({
                         headerLeft: () => (
@@ -225,126 +225,126 @@ export default function App() {
                       })}
                     />
                     <Stack.Screen
-                      name="GameMaterials"
+                      name='GameMaterials'
                       component={GameMaterials}
                       options={{
-                        title: "Choose your game!",
+                        title: 'Choose your game!',
                       }}
                     />
                     <Stack.Screen
-                      name="Gameplay"
+                      name='Gameplay'
                       options={({ navigation }) => ({
                         headerRight: () => (
                           <PauseButton
                             onPress={() => {
                               setPaused(true);
-                              navigation.navigate("Pause");
+                              navigation.navigate('Pause');
                             }}
                           />
                         ),
-                        title: "Math",
+                        title: 'Math',
                       })}
                     >
                       {(props) => <Gameplay {...props} paused={paused} />}
                     </Stack.Screen>
                     <Stack.Screen
-                      name="GameplayIntermediate"
+                      name='GameplayIntermediate'
                       component={GameplayIntermediate}
                       options={{
-                        title: "Gameplay Intermediate",
+                        title: 'Gameplay Intermediate',
                       }}
                     />
                     <Stack.Screen
-                      name="TriviaScreen"
+                      name='TriviaScreen'
                       component={TriviaScreen}
                       options={({ navigation }) => ({
                         headerRight: () => (
                           <PauseButton
-                            onPress={() => navigation.navigate("Pause")}
+                            onPress={() => navigation.navigate('Pause')}
                           />
                         ),
-                        title: "Writing (Trivia)",
+                        title: 'Writing (Trivia)',
                       })}
                     />
                     <Stack.Screen
-                      name="FinishedScreen"
+                      name='FinishedScreen'
                       component={FinishedScreen}
                       options={{
-                        title: "Exercises Completed",
+                        title: 'Exercises Completed',
                       }}
                     />
                     <Stack.Screen
-                      name="WritingIntro"
+                      name='WritingIntro'
                       component={WritingIntro}
                       options={{
-                        title: "Writing",
+                        title: 'Writing',
                       }}
                     />
                     <Stack.Screen
-                      name="MathIntro"
+                      name='MathIntro'
                       component={MathIntro}
                       options={{
-                        title: "Math",
+                        title: 'Math',
                       }}
                     />
                     <Stack.Screen
-                      name="TriviaIntro"
+                      name='TriviaIntro'
                       component={TriviaIntro}
                       options={{
-                        title: "Writing (Trivia)",
+                        title: 'Writing (Trivia)',
                       }}
                     />
                     <Stack.Screen
-                      name="ExtraPractice"
+                      name='ExtraPractice'
                       component={ExtraPractice}
                       options={{
-                        title: "More Exercises",
+                        title: 'More Exercises',
                       }}
                     />
                     <Stack.Screen
-                      name="ReadingIntro"
+                      name='ReadingIntro'
                       component={ReadingIntro}
                       options={{
-                        title: "Get ready to read!",
+                        title: 'Get ready to read!',
                       }}
                     />
                     <Stack.Screen
-                      name="ReadingMain"
+                      name='ReadingMain'
                       component={ReadingMain}
                       options={({ navigation }) => ({
                         headerRight: () => (
                           <PauseButton
-                            onPress={() => navigation.navigate("Pause")}
+                            onPress={() => navigation.navigate('Pause')}
                           />
                         ),
-                        title: "Reading",
+                        title: 'Reading',
                       })}
                     />
                     <Stack.Screen
-                      name="PromptScreen"
+                      name='PromptScreen'
                       component={PromptScreen}
                       options={({ navigation }) => ({
                         headerRight: () => (
                           <PauseButton
-                            onPress={() => navigation.navigate("Pause")}
+                            onPress={() => navigation.navigate('Pause')}
                           />
                         ),
-                        title: "Writing Prompts",
+                        title: 'Writing Prompts',
                       })}
                     />
                     <Stack.Screen
-                      name="ExercisesCompleted"
+                      name='ExercisesCompleted'
                       component={ExercisesCompleted}
                       options={{
-                        title: "Exercises done!",
+                        title: 'Exercises done!',
                       }}
                     />
                     {/* Pause Screen */}
                     <Stack.Screen
-                      name="Pause"
+                      name='Pause'
                       options={{
-                        title: "Paused",
-                        animationTypeForReplace: "pop",
+                        title: 'Paused',
+                        animationTypeForReplace: 'pop',
                         transitionSpec: {
                           open: config,
                           close: config,
@@ -356,7 +356,7 @@ export default function App() {
 
                     {/* Settings Screens */}
                     <Stack.Screen
-                      name="SettingsScreen"
+                      name='SettingsScreen'
                       component={SettingsScreen}
                       options={({ navigation }) => ({
                         headerLeft: () => (
@@ -364,11 +364,11 @@ export default function App() {
                             onPress={() => navigation.goBack()}
                           />
                         ),
-                        title: "Settings",
+                        title: 'Settings',
                       })}
                     />
                     <Stack.Screen
-                      name="TimePicker"
+                      name='TimePicker'
                       component={TimePicker}
                       options={({ navigation }) => ({
                         headerLeft: () => (
@@ -376,11 +376,11 @@ export default function App() {
                             onPress={() => navigation.goBack()}
                           />
                         ),
-                        title: "Set Time Reminder",
+                        title: 'Set Time Reminder',
                       })}
                     />
                     <Stack.Screen
-                      name="SoundScreen"
+                      name='SoundScreen'
                       component={SoundScreen}
                       options={({ navigation }) => ({
                         headerLeft: () => (
@@ -388,11 +388,11 @@ export default function App() {
                             onPress={() => navigation.goBack()}
                           />
                         ),
-                        title: "Sound",
+                        title: 'Sound',
                       })}
                     />
                     <Stack.Screen
-                      name="FontSize"
+                      name='FontSize'
                       component={FontSize}
                       options={({ navigation }) => ({
                         headerLeft: () => (
@@ -400,11 +400,11 @@ export default function App() {
                             onPress={() => navigation.goBack()}
                           />
                         ),
-                        title: "Font Size",
+                        title: 'Font Size',
                       })}
                     />
                     <Stack.Screen
-                      name="StreakLength"
+                      name='StreakLength'
                       component={StreakLength}
                       options={({ navigation }) => ({
                         headerLeft: () => (
@@ -412,7 +412,7 @@ export default function App() {
                             onPress={() => navigation.goBack()}
                           />
                         ),
-                        title: "Streak Length",
+                        title: 'Streak Length',
                       })}
                     />
                   </Stack.Navigator>
