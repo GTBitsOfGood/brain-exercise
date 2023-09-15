8240062cce9effd2b3f9f1acf4e3d8f4c7eb459e
cdeng65 <cdeng65@gatech.edu>
2023-09-15 01:50:29 -0400

    lint and format

--- src/hooks/useTriviaProblems.ts
+++ src/hooks/useTriviaProblems.ts
@@ -1,6 +1,9 @@
 import { useCallback, useState } from "react";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types";
 
-export default function useTriviaProblems() {
+type Props = NativeStackScreenProps<RootStackParamList, "TriviaMain">;
+export default function useTriviaProblems({ navigation, route }: Props) {
   const [questionsAttempted, setQuestionsAttempted] = useState(0);
   const [questionsCorrect, setQuestionsCorrect] = useState(0);
   const [totalTime, setTotalTime] = useState(0);
@@ -18,16 +21,23 @@ export default function useTriviaProblems() {
 
   const onTimeComplete = useCallback(() => {
     const average = questionsAttempted > 0 ? totalTime / questionsAttempted : 0;
+    navigation.replace(...route.params.nextScreenArgs);
 
     return {
       questionsAttempted,
       questionsCorrect,
-      timePerQuestion: average
+      timePerQuestion: average,
     };
-  }, [questionsAttempted, questionsCorrect, totalTime]); 
+  }, [
+    questionsAttempted,
+    questionsCorrect,
+    totalTime,
+    navigation,
+    route.params.nextScreenArgs,
+  ]);
 
-  return { 
-    updateStatsOnAnswer, 
-    onTimeComplete
+  return {
+    updateStatsOnAnswer,
+    onTimeComplete,
   };
 }
--- src/screens/Game/TriviaMain.tsx
+++ src/screens/Game/TriviaMain.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useRef  } from "react";
+import { useState, useCallback, useRef } from "react";
 import { StyleSheet, View } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import "react-native-gesture-handler";
@@ -41,28 +41,28 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   container: {
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   showButton: {
     marginBottom: 20,
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   yesButton: {
     marginBottom: 20,
     marginRight: 10,
-    width: 150
-},
-noButton: {    
-  marginBottom: 20,
+    width: 150,
+  },
+  noButton: {
+    marginBottom: 20,
     marginLeft: 10,
-    width: 150
-},
+    width: 150,
+  },
   buttonTitle: {
-    color: 'white'
+    color: "white",
   },
   showButtonTitle: {
     alignSelf: "center",
@@ -77,18 +77,19 @@ type Props = NativeStackScreenProps<RootStackParamList, "TriviaMain">;
 export default function TriviaScreen({ navigation, route }: Props) {
   const [problem, setProblem] = useState(getProblem());
   const [answered, setAnswered] = useState(false);
-  const { updateStatsOnAnswer, onTimeComplete } = useTriviaProblems();
+  const { updateStatsOnAnswer, onTimeComplete } = useTriviaProblems({
+    navigation,
+    route,
+  });
 
   const elapsedTimeRef = useRef<number>(0);
   const remainingTimeRef = useRef<RemainingTimeGetter>();
 
-
-
-  const getNewProblem = () => {
+  const getNewProblem = useCallback(() => {
     setProblem(getProblem());
     setAnswered(false);
-    elapsedTimeRef.current = 0; 
-  };
+    elapsedTimeRef.current = 0;
+  }, []);
 
   const onPressShowButton = useCallback(() => {
     if (!answered) {
@@ -96,71 +97,67 @@ export default function TriviaScreen({ navigation, route }: Props) {
     } else {
       getNewProblem();
     }
-  }, [answered]);
+  }, [answered, getNewProblem]);
 
-  
   const onPressYesButton = useCallback(() => {
-    elapsedTimeRef.current = remainingTimeRef.current.getRemainingTime() - TOTAL_TIME;
+    elapsedTimeRef.current =
+      remainingTimeRef.current.getRemainingTime() - TOTAL_TIME;
     updateStatsOnAnswer(true, elapsedTimeRef.current);
-    getNewProblem(); 
+    getNewProblem();
   }, [getNewProblem, updateStatsOnAnswer]);
 
   const onPressNoButton = useCallback(() => {
-    elapsedTimeRef.current = remainingTimeRef.current.getRemainingTime() - TOTAL_TIME;
+    elapsedTimeRef.current =
+      remainingTimeRef.current.getRemainingTime() - TOTAL_TIME;
     updateStatsOnAnswer(false, elapsedTimeRef.current);
-    setAnswered(true); 
-    getNewProblem(); 
-  }, [updateStatsOnAnswer]);
-
-
+    setAnswered(true);
+    getNewProblem();
+  }, [getNewProblem, updateStatsOnAnswer]);
 
   return (
     <View style={styles.root}>
-        <View>
-            <ProgressBar
-                maxSeconds={TOTAL_TIME}
-                remainingTimeRef={remainingTimeRef}  
-                redThreshold={60}
-                onTimeComplete={onTimeComplete}
-            />
-            <Text style={styles.instructionText}>
-                Write the question, then your answer
-            </Text>
-        </View>
-        <Text style={styles.questionText}>{problem.question}</Text>
+      <View>
+        <ProgressBar
+          maxSeconds={TOTAL_TIME}
+          remainingTimeRef={remainingTimeRef}
+          redThreshold={60}
+          onTimeComplete={onTimeComplete}
+        />
+        <Text style={styles.instructionText}>
+          Write the question, then your answer
+        </Text>
+      </View>
+      <Text style={styles.questionText}>{problem.question}</Text>
 
-        {answered ? (
-            <>
-                <Text style={styles.answerText}>Answer:</Text>
-                <Text style={styles.actualAnswerText}>
-                    {String(problem.answer)}
-                </Text>
-                <Text style={styles.questionText}>Did you answer it correctly?</Text>
+      {answered ? (
+        <>
+          <Text style={styles.answerText}>Answer:</Text>
+          <Text style={styles.actualAnswerText}>{String(problem.answer)}</Text>
+          <Text style={styles.questionText}>Did you answer it correctly?</Text>
 
-                <View style={styles.buttonContainer}>
-                    <Button
-                        title="Yes"
-                        buttonStyle={styles.yesButton}
-                        titleStyle={styles.buttonTitle}
-                        onPress={onPressYesButton}
-                    />
-                    <Button
-                        title="No"
-                        buttonStyle={styles.noButton}
-                        titleStyle={styles.buttonTitle}
-                        onPress={onPressNoButton}
-                    />
-                </View>
-            </>
-        ) : (
+          <View style={styles.buttonContainer}>
             <Button
-                title="Show Answer"
-                buttonStyle={styles.showButton}
-                titleStyle={styles.showButtonTitle}
-                onPress={onPressShowButton}
+              title="Yes"
+              buttonStyle={styles.yesButton}
+              titleStyle={styles.buttonTitle}
+              onPress={onPressYesButton}
             />
-        )}
+            <Button
+              title="No"
+              buttonStyle={styles.noButton}
+              titleStyle={styles.buttonTitle}
+              onPress={onPressNoButton}
+            />
+          </View>
+        </>
+      ) : (
+        <Button
+          title="Show Answer"
+          buttonStyle={styles.showButton}
+          titleStyle={styles.showButtonTitle}
+          onPress={onPressShowButton}
+        />
+      )}
     </View>
-);
-
+  );
 }
