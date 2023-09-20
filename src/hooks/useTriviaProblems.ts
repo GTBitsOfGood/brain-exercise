0589de685ec97f989c597f23e0eea8104318ff28
Jeffrey Deng <jeffreydcz@gatech.edu>
2023-09-19 22:02:21 -0400

    Trivia analytics (#191)
    * hooks and game data
    
    * remove ios
    
    * lint and format
    
    * fixes
    
    * lint and format

---
+++ src/hooks/useTriviaProblems.ts
@@ -0,0 +1,42 @@
+import { useCallback, useState } from "react";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types";
+
+import gameDescriptions from "../screens/Stacks/gameDescriptions";
+
+type Props = NativeStackScreenProps<RootStackParamList, "TriviaMain">;
+
+const TOTAL_TIME = gameDescriptions.Trivia.minutes * 60;
+export default function useTriviaProblems({ navigation, route }: Props) {
+  const [questionsAttempted, setQuestionsAttempted] = useState(0);
+  const [questionsCorrect, setQuestionsCorrect] = useState(0);
+
+  const updateStatsOnAnswer = useCallback((isCorrect: boolean) => {
+    setQuestionsAttempted((prev) => prev + 1);
+    if (isCorrect) {
+      setQuestionsCorrect((prev) => prev + 1);
+    }
+  }, []);
+
+  const onTimeComplete = useCallback(() => {
+    const average =
+      questionsAttempted > 0 ? TOTAL_TIME / questionsAttempted : 0;
+    navigation.replace(...route.params.nextScreenArgs);
+
+    return {
+      questionsAttempted,
+      questionsCorrect,
+      timePerQuestion: average,
+    };
+  }, [
+    questionsAttempted,
+    questionsCorrect,
+    navigation,
+    route.params.nextScreenArgs,
+  ]);
+
+  return {
+    updateStatsOnAnswer,
+    onTimeComplete,
+  };
+}
--- src/screens/Game/TriviaMain.tsx
+++ src/screens/Game/TriviaMain.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef } from "react";
 import { StyleSheet, View } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import "react-native-gesture-handler";
@@ -7,7 +7,9 @@ import Button from "../../components/Button";
 import ProgressBar from "../../components/ProgressBar";
 import Text from "../../components/Text";
 import gameDescriptions from "../Stacks/gameDescriptions";
-import { RootStackParamList } from "../../types";
+import { RemainingTimeGetter, RootStackParamList } from "../../types";
+
+import useTriviaProblems from "../../hooks/useTriviaProblems";
 
 const styles = StyleSheet.create({
   root: {
@@ -38,8 +40,29 @@ const styles = StyleSheet.create({
     fontSize: 25,
     textAlign: "center",
   },
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
   showButton: {
     marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  yesButton: {
+    marginBottom: 20,
+    marginRight: 10,
+    width: 150,
+  },
+  noButton: {
+    marginBottom: 20,
+    marginLeft: 10,
+    width: 150,
+  },
+  buttonTitle: {
+    color: "white",
   },
   showButtonTitle: {
     alignSelf: "center",
@@ -51,14 +74,19 @@ const styles = StyleSheet.create({
 const TOTAL_TIME = gameDescriptions.Trivia.minutes * 60;
 type Props = NativeStackScreenProps<RootStackParamList, "TriviaMain">;
 
-function TriviaScreen({ navigation, route }: Props) {
+export default function TriviaScreen({ navigation, route }: Props) {
   const [problem, setProblem] = useState(getProblem());
   const [answered, setAnswered] = useState(false);
+  const { updateStatsOnAnswer, onTimeComplete } = useTriviaProblems({
+    navigation,
+    route,
+  });
+  const remainingTimeRef = useRef<RemainingTimeGetter>();
 
-  const getNewProblem = () => {
+  const getNewProblem = useCallback(() => {
     setProblem(getProblem());
     setAnswered(false);
-  };
+  }, []);
 
   const onPressShowButton = useCallback(() => {
     if (!answered) {
@@ -66,17 +94,22 @@ function TriviaScreen({ navigation, route }: Props) {
     } else {
       getNewProblem();
     }
-  }, [answered]);
+  }, [answered, getNewProblem]);
 
-  const onTimeComplete = useCallback(() => {
-    navigation.navigate(...route.params.nextScreenArgs);
-  }, [navigation, route.params.nextScreenArgs]);
+  const onAnswerClick = useCallback(
+    (isCorrect: boolean) => {
+      updateStatsOnAnswer(isCorrect);
+      getNewProblem();
+    },
+    [getNewProblem, updateStatsOnAnswer],
+  );
 
   return (
     <View style={styles.root}>
       <View>
         <ProgressBar
           maxSeconds={TOTAL_TIME}
+          remainingTimeRef={remainingTimeRef}
           redThreshold={60}
           onTimeComplete={onTimeComplete}
         />
@@ -85,23 +118,36 @@ function TriviaScreen({ navigation, route }: Props) {
         </Text>
       </View>
       <Text style={styles.questionText}>{problem.question}</Text>
-      <View>
-        <Text style={styles.answerText}>{answered ? "Answer:" : ""}</Text>
-        <Text style={styles.actualAnswerText}>
-          {answered ? `${problem.answer}` : ""}
-        </Text>
-      </View>
-      <View>
+
+      {answered ? (
+        <>
+          <Text style={styles.answerText}>Answer:</Text>
+          <Text style={styles.actualAnswerText}>{String(problem.answer)}</Text>
+          <Text style={styles.questionText}>Did you answer it correctly?</Text>
+
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Yes"
+              buttonStyle={styles.yesButton}
+              titleStyle={styles.buttonTitle}
+              onPress={() => onAnswerClick(true)}
+            />
+            <Button
+              title="No"
+              buttonStyle={styles.noButton}
+              titleStyle={styles.buttonTitle}
+              onPress={() => onAnswerClick(false)}
+            />
+          </View>
+        </>
+      ) : (
         <Button
-          title={answered ? "Next" : "Show Answer"}
+          title="Show Answer"
           buttonStyle={styles.showButton}
           titleStyle={styles.showButtonTitle}
-          shouldNotPlay
           onPress={onPressShowButton}
         />
-      </View>
+      )}
     </View>
   );
 }
-
-export default TriviaScreen;
