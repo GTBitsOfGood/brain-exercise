761737ef7336af220eff343c74850b2dfb2d8b57
Johannes Qian <johannesq23@gmail.com>
2024-11-02 22:29:36 -0400

    Do not need to add chapter actions

--- src/actions/Chapter.ts
+++
@@ -1,8 +0,0 @@
-import ChapterModel from "../models/Chapter";
-import dbConnect from "./dbConnect";
-
-export async function getAllChapters() {
-  await dbConnect();
-  console.log(ChapterModel.find());
-  return ChapterModel.find();
-}
